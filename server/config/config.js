96241fc8a8b11593dce757cfbeb2a51a4e8e8c64
yoshua <yoshua10@hotmail.com>
2020-03-15 13:18:03 +0100

    Variable entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Yoshua:Camello9@cluster0-zykor.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
